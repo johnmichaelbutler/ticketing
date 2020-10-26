2ce18895a6363544b783d4f4401efc9446c24853
John Butler <johnmichaelbutler@yahoo.com>
2020-10-26 14:09:28 -0500

    Update to ticket-updated-listener

--- orders/src/events/listeners/ticket-updated-listener.ts
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -21,28 +21,3 @@ export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
     msg.ack();
   }
 }
-
-
-// export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
-//   readonly subject = Subjects.TicketUpdated;
-//   queueGroupName = queueGroupName;
-
-//   async onMessage(data: TicketUpdatedEvent['data'], msg: Message) {
-//     try {
-//       const ticket = await Ticket.findByEvent(data);
-//       console.log('Data for ticket-updated-listener from Ticket service', data);
-//       console.log('ticket from ticket-updated-listener', ticket);
-//       if (!ticket) {
-//         throw new Error('Ticket not found');
-//       }
-//       const { title, price } = data;
-//       ticket!.set({ title, price });
-//       await ticket!.save();
-//     } catch (error) {
-//       console.log(error);
-//     }
-
-
-//     msg.ack();
-//   }
-// }
