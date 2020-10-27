b88cfe3d597253397b3d385ac0270d055dd48e7a
John Butler <johnmichaelbutler@yahoo.com>
2020-10-27 13:00:33 -0500

    Fixed type in build-client.js

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 const buildClient = ({ req }) => {
+  console.log('server_base_url', process.env.SERVER_URL_BASE);
   if (typeof window === 'undefined') {
     // We are on the server
     return axios.create({
-      baseURL: process.env.SERVER_BASE_URL,
+      baseURL: process.env.SERVER_URL_BASE,
       headers: req.headers,
     });
   } else {
